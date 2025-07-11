72abb15122a54492d475108fc778f2c2c8db08d0
Fernando Sousa <afernandosousab@gmail.com>
2025-07-11 17:24:54 -0300

    update

--- aula39.py
+++ aula39.py
@@ -5,6 +5,18 @@ Iterando strings com while
 # nome = 'Luiz Otávio'  # Iteráveis
 #      1110987654321
 
+nome_fernando = 'Fernando'
+indice_fernando = 0
+novo_nome_fernando = ''
+
+while indice_fernando < len(nome_fernando):
+    letra_fernando = nome_fernando[indice_fernando]
+    novo_nome_fernando += f'*{letra_fernando}'
+    indice_fernando += 1
+
+
+novo_nome_fernando += '*'
+print(novo_nome_fernando)
 
 nome = 'Maria Helena'  # Iteráveis
 
---
+++ testes avulsos.py
@@ -0,0 +1,76 @@
+# frase = 'aaaooo'
+# i = 0
+
+# qtd_mais_vezes = 0
+# letra_que_mais_apareceu = ''
+
+# while i < len(frase):
+#     letra_atual = frase[i]
+
+#     if letra_atual == 'o' or letra_atual == ' ' :
+#         i+=1
+#         continue
+
+#     qtd_apareceu_atual = frase.count(letra_atual)
+
+#     if qtd_mais_vezes < qtd_apareceu_atual:
+#         qtd_mais_vezes = qtd_apareceu_atual
+#         letra_que_mais_apareceu = letra_atual
+
+#     print(letra_atual, qtd_apareceu_atual)
+
+#     i+=1
+
+# print(f'A letra que mais apareceu foi "{letra_que_mais_apareceu}" {qtd_mais_vezes}x')
+
+
+
+
+
+# Texto_teste = 'Fernando'
+# iterador = iter(Texto_teste)
+
+# while True:
+#     try:
+#         letra = next(iterador)
+#         print(letra)
+#     except StopIteration:
+#         break        
+
+
+
+palavra_secreta = 'amoreco'
+letras_acertadas = ''
+qtd_tentativas = 0
+
+while True:
+    letra_digitada = input('Escolha uma letra: ')
+    qtd_tentativas += 1
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra')
+        continue
+
+    if len(letra_digitada) == 0:
+        print('Digite a letra')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += ('*')
+    print(palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        print('O jogo acabou !')
+
+    continue
+
+
+
+    
